909e88ee71d21aff49750df9b83d12d682a1cf74
lucastefanescu <lucastefanescu383@gmail.com>
2024-09-15 12:53:55 -0400

    created and started to style quote form

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -4,17 +4,19 @@ function Quote() {
 		<>
 			<div className="main-quote-content">
 				<form className="quote-form" action="#" method="POST">
-					<h1>GET A QUOTE</h1>
-					<label htmlFor="name"> Name </label>
-					<input type="Name" id="name"></input>
-					<label htmlFor="email">Email</label>
-					<input type="email" id="email"></input>
-					<label htmlFor="phone-number">Phone number</label>
-					<input type="tel" id="phone-number"></input>
-					<label htmlFor="description">Description</label>
-					<input type="text" id="description"></input>
-					<label htmlFor="photos">Photos</label>
-					<input type="file" id="photos" multiple></input>
+					<div className="container">
+						<h1>GET A QUOTE</h1>
+						<label htmlFor="name"> Name </label>
+						<input type="Name" id="name"></input>
+						<label htmlFor="email">Email</label>
+						<input type="email" id="email"></input>
+						<label htmlFor="phone-number">Phone number</label>
+						<input type="tel" id="phone-number"></input>
+						<label htmlFor="description">Description</label>
+						<input type="text" id="description"></input>
+						<label htmlFor="photos">Photos</label>
+						<input type="file" id="photos" multiple></input>
+					</div>
 				</form>
 			</div>
 		</>
