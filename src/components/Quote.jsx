eba9dce00250501611cbc4ce786d23329de7a5f2
lucastefanescu <lucastefanescu383@gmail.com>
2024-09-18 10:13:19 -0400

    finished quote page

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -7,13 +7,21 @@ function Quote() {
 					<div className="container">
 						<h1>GET A QUOTE</h1>
 						<label htmlFor="name"> Name </label>
-						<input type="Name" id="name"></input>
+						<input type="Name" id="name" placeholder="Enter name"></input>
 						<label htmlFor="email">Email</label>
-						<input type="email" id="email"></input>
+						<input type="email" id="email" placeholder="Enter email"></input>
 						<label htmlFor="phone-number">Phone Number</label>
-						<input type="tel" id="phone-number"></input>
+						<input
+							type="tel"
+							id="phone-number"
+							placeholder="Enter phone number"
+						></input>
 						<label htmlFor="description">Job Description</label>
-						<input type="text" id="description"></input>
+						<input
+							type="text"
+							id="description"
+							placeholder="Enter job description"
+						></input>
 						<button className="btn">SUBMIT</button>
 					</div>
 				</form>
