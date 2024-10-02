67662185482e4621f4ea202e075e45f02d84bc34
lucastefanescu <lucastefanescu383@gmail.com>
2024-10-02 00:25:49 -0400

    started working on slideshow for before/after

--- src/components/Before.jsx
+++ src/components/Before.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../styling/Before.css";
 import image1 from "../pictures/slideshowimage1.jpg";
 import image2 from "../pictures/slideshowimage2.jpg";
 
@@ -7,7 +8,7 @@ const images = [image1, image2];
 function Before() {
 	return (
 		<>
-			<div className="slideshow">
+			<div className="slideshow-slider">
 				<div className="current-slide">
 					{images.map((picture, idx) => (
 						<img className="slide" key={idx} src={picture} />
